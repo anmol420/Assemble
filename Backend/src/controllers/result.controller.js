35f3a90b7f54c4b65033a35eaf32ed081d9ca224
anmol420 <anmolanand2k15@gmail.com>
2025-02-03 22:09:24 +0530

    Added Minor Fix

--- Backend/src/controllers/result.controller.js
+++ Backend/src/controllers/result.controller.js
@@ -4,37 +4,26 @@ import ApiResponse from "../utils/ApiResponse.js";
 
 import { Result } from "../models/result.models.js";
 import { Tournament } from "../models/tournament.models.js";
-import { User } from "../models/user.models.js";
 
-const postResult = asyncHandler(async (req, res) => {
-    const { tournamentName, leaderboard } = req.body;
-
+const getResults = asyncHandler(async (req, res) => {
+    const { tournamentName } = req.body;
     const tournament = await Tournament.findOne({ name: tournamentName });
     if (!tournament) {
         throw new ApiError(404, "Tournament not found");
     }
-
-    // if (!tournament.isActive) {
-    //     throw new ApiError(400, "Tournament is not active");
-    // }
-
-    console.log(leaderboard);
-    
-
-    try {
-        await Result.create({
-            tournament: tournament._id,
-            leaderboard,
-        });
-
-        return res
-            .status(201)
-            .json(new ApiResponse(201, "Result Posted Successfully!!"));
-        } catch (error) {
-            throw new ApiError(500, error.message || "Internal Server Error");
-        }
-    });
-
-    export {
-        postResult,
+    const results = await Result.find({ tournament: tournament._id })
+    if (!results) {
+        throw new ApiError(404, "Results not found");
     }
+    try {
+        return res
+            .status(200)
+            .json(new ApiResponse(200, results, "Results fetched successfully!!"));
+    } catch (error) {
+        throw new ApiError(500, error.message);
+    }
+});
+
+export {
+    getResults
+};
